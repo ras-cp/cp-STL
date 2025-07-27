84d7e777bfa5adf0fde026a38110a64ad390e250
ras-cp <ras1129cp@gmail.com>
2025-07-27 21:24:58 +0900

    Update and rename lc_dsu.test.cpp to lc-Union-Find-Dsu.test.cpp

--- verify/ds/lc-Union-Find-Dsu.test.cpp
+++ verify/ds/lc-Union-Find-Dsu.test.cpp
@@ -1,17 +1,17 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 
 #include <bits/stdc++.h>
-#include "cpstl/other/fastio.hpp"
-#include "cpstl/ds/dsu.hpp"
+#include "cpstl/other/Fastio.hpp"
+#include "cpstl/ds/Dsu.hpp"
 
 int main() {
 	int N, Q;
 	cpstd::input(N, Q);
-	cpstd::dsu uf(N);
+	cpstd::Dsu dsu(N);
 	int t, u, v;
 	while (Q--) {
 		cpstd::input(t, u, v);
-		if (t == 0) uf.merge(u, v);
-		else cpstd::print((uf.same(u, v) ? "1" : "0"));
+		if (t == 0) dsu.merge(u, v);
+		else cpstd::print((dsu.same(u, v) ? "1" : "0"));
 	}
 }
